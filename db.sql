fc5207657b82f304bc2a8d8485db34ca096cfc08
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2021-03-26 16:34:40 +0430

    Fix the some errors

--- db.sql
+++ db.sql
@@ -1,18 +1,19 @@
-CREATE TABLE `users`{
-    `id` INT NOT NULL PRIMARY KEY UNIQUE AUTO_INCREMENT,
+CREATE TABLE `users`(
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
     `username` VARCHAR(20) NOT NULL UNIQUE,
     `email` VARCHAR(255) NOT NULL UNIQUE,
     `password` VARCHAR(255) NOT NULL,
     `isAdmin` ENUM('Y', 'N') DEFAULT 'N',
     `isAccountEnable` ENUM('Y', 'N') DEFAULT 'Y'
-    };
-CREATE TABLE `settings`{
-    `siteName` VARCHAR NOT NULL,
-    `siteDescription` VARCHAR NOT NULL
-    };
-CREATE TABLE `todos`{
-    `id` INT UNIQUE PRIMARY KEY NOT NULL AUTO_INCREMENT,
+    );
+CREATE TABLE `settings`(
+    `siteName` VARCHAR(50) NOT NULL,
+    `siteDescription` TEXT NOT NULL
+);
+CREATE TABLE `todos`(
+    `id` INT PRIMARY KEY AUTO_INCREMENT,
     `userId` INT NOT NULL,
-    `todo` VARCHAR NOT NULL,
+    `todo` TEXT NOT NULL,
     `isDone` ENUM('Y', 'N') DEFAULT 'N'
-    };
+    );
+commit;
