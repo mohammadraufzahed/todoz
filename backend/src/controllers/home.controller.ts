ecc330c02958f6c2f7dd92c02e8cb0dbd6d24975
Mohammad Raufzahed <mohammadraufzahed@protonmail.com>
2021-12-25 12:03:14 +0330

    Deleted the log command

--- backend/src/controllers/home.controller.ts
+++ backend/src/controllers/home.controller.ts
@@ -1,7 +1,6 @@
 import { Request, ResponseToolkit } from "@hapi/hapi";
 
 export async function homeHandler(req: Request, res: ResponseToolkit) {
-  console.dir(req.query);
   if (req.query.name) {
     return `Hi ${req.query.name}`;
   }
